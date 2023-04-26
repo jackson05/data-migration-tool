b1b9f09be1e796e4500eb79713bd2ed5cea5fb93
jackson05 <jackson5g.bi@gmail.com>
2023-04-27 00:34:52 +0200

    git status

---
+++ src/launcher/Connections.java
@@ -0,0 +1,24 @@
+package launcher;
+
+import java.util.List;
+
+import com.mysql.jdbc.Connection;
+
+public class Connections {
+	
+	
+	public Connection getSQLConnection(String sQLdatabase) {
+		
+		
+		return null;
+	}
+
+	public Connection getMySQLConnection(String mySQLDatabase) {
+		
+		return null;
+	}
+	
+	public List<String> getDataBases() {
+		return null;
+	}
+}
