b8aa4b80e6945af0f14ef4bfef166408a86f9c84
jackson05 <jackson5g.bi@gmail.com>
2023-04-26 12:55:57 +0200

    transaction managment and connection managed

--- src/launcher/SQLServerToMySQLMigration.java
+++ src/launcher/SQLServerToMySQLMigration.java
@@ -13,89 +13,113 @@ public class SQLServerToMySQLMigration {
         String mySqlUser = "openclinic";
         String mySqlPassword = "0pen";
         
-        try (Connection sqlServerConnection = DriverManager.getConnection(sqlServerUrl, sqlServerUser, sqlServerPassword);
-             Connection mySqlConnection = DriverManager.getConnection(mySqlUrl, mySqlUser, mySqlPassword)) 
-        {
-        	
-        	mySqlConnection.setAutoCommit(false);
-            // Récupérer les noms de toutes les tables SQL Server
-            DatabaseMetaData metaData = sqlServerConnection.getMetaData();
-            ResultSet tablesResultSet = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        
+        try {
+    	   
+        	Connection sQLServerConnection = DriverManager.getConnection(sqlServerUrl, sqlServerUser, sqlServerPassword);
             
-            while (tablesResultSet.next()) {
-            	
-                String tableName = tablesResultSet.getString("TABLE_NAME");
-                
-                System.out.println("======> working on "+tableName);
-                //System.out.println("Traitement de la table : " + tableName);
-                
-                /* Vérifier si la table existe dans MySQL
-                boolean tableExists = false;
-                ResultSet mySqlTablesResultSet = mySqlConnection.getMetaData().getTables(null, null, tableName, null);
-                while (mySqlTablesResultSet.next()) {
-                    tableExists = true;
-                }
-                /*
-                
-                 if (tableExists) {
-                    // Créer la table dans MySQL en utilisant les informations de la table SQL Server
-                    ResultSet columnsResultSet = metaData.getColumns(null, null, tableName, "%");
-                    StringBuilder createTableQuery = new StringBuilder("CREATE TABLE " + tableName + " (");
-                    boolean isFirstColumn = true;
-                    
-                    System.out.print("|");
-                    while (columnsResultSet.next()) {
-                        if (!isFirstColumn) {
-                            createTableQuery.append(", ");
-                        }
-                        createTableQuery.append(columnsResultSet.getString("COLUMN_NAME"))
-                                       .append(" ")
-                                       .append(columnsResultSet.getString("TYPE_NAME"))
-                                       .append(("SIZE ()"));
-                        isFirstColumn = false;
-                        
-                        System.out.print(columnsResultSet.getString("COLUMN_NAME")+"("+columnsResultSet.getString("TYPE_NAME")+") SIZE ("
-                        		+ columnsResultSet.getFetchSize()+")|");
-                    }
-                    createTableQuery.append(")");
-                    System.out.println("Création de la table : " + createTableQuery.toString());
-                   // PreparedStatement createTableStatement = mySqlConnection.prepareStatement(createTableQuery.toString());
-                    //createTableStatement.executeUpdate();
-                }
-                
-                */
-                
-                // Copier les données de la table SQL Server vers MySQL
-                PreparedStatement selectStatement = sqlServerConnection.prepareStatement("SELECT * FROM " + tableName);
-                ResultSet dataResultSet = selectStatement.executeQuery();
-                ResultSetMetaData dataResultSetMetaData = dataResultSet.getMetaData();
-                StringBuilder insertQuery = new StringBuilder("INSERT INTO " + tableName + " VALUES (");
-                for (int i = 1; i <= dataResultSetMetaData.getColumnCount(); i++) {
-                    if (i > 1) {
-                        insertQuery.append(", ");
-                    }
-                    insertQuery.append("?");
-                    
-                }
-                insertQuery.append(")");
-               // System.out.println("Debut d'Insertion des données : " + insertQuery.toString());
-                PreparedStatement insertStatement = mySqlConnection.prepareStatement(insertQuery.toString());
-                
-                while (dataResultSet.next()) {
-                    for (int i = 1; i <= dataResultSetMetaData.getColumnCount(); i++) {
-                        insertStatement.setObject(i, dataResultSet.getObject(i));
-                    
-                    }
-                    
-               //   insertStatement.executeUpdate();
-                    mySqlConnection.commit();
-                } 
-            } 
+           // Récupérer les noms de toutes les tables SQL Server
+           DatabaseMetaData sQLMetaData = sQLServerConnection.getMetaData();
+           ResultSet sQLTablesResultSet = sQLMetaData.getTables(null, null, "%", new String[]{"TABLE"});
+           
+           while (sQLTablesResultSet.next()) {
+           	
+               String tableName = sQLTablesResultSet.getString("TABLE_NAME");
+               
+               System.out.println("======> working on "+tableName);
+               //System.out.println("Traitement de la table : " + tableName);
+               
+               /* Vérifier si la table existe dans MySQL
+               boolean tableExists = false;
+               ResultSet mySqlTablesResultSet = mySqlConnection.getMetaData().getTables(null, null, tableName, null);
+               while (mySqlTablesResultSet.next()) {
+                   tableExists = true;
+               }
+               /*
+               
+                if (tableExists) {
+                   // Créer la table dans MySQL en utilisant les informations de la table SQL Server
+                   ResultSet columnsResultSet = metaData.getColumns(null, null, tableName, "%");
+                   StringBuilder createTableQuery = new StringBuilder("CREATE TABLE " + tableName + " (");
+                   boolean isFirstColumn = true;
+                   
+                   System.out.print("|");
+                   while (columnsResultSet.next()) {
+                       if (!isFirstColumn) {
+                           createTableQuery.append(", ");
+                       }
+                       createTableQuery.append(columnsResultSet.getString("COLUMN_NAME"))
+                                      .append(" ")
+                                      .append(columnsResultSet.getString("TYPE_NAME"))
+                                      .append(("SIZE ()"));
+                       isFirstColumn = false;
+                       
+                       System.out.print(columnsResultSet.getString("COLUMN_NAME")+"("+columnsResultSet.getString("TYPE_NAME")+") SIZE ("
+                       		+ columnsResultSet.getFetchSize()+")|");
+                   }
+                   createTableQuery.append(")");
+                   System.out.println("Création de la table : " + createTableQuery.toString());
+                  // PreparedStatement createTableStatement = mySqlConnection.prepareStatement(createTableQuery.toString());
+                   //createTableStatement.executeUpdate();
+               }
+               
+               */
+               
+               // Copier les données de la table SQL Server vers MySQL
+               PreparedStatement sQLPreparedStatement = sQLServerConnection.prepareStatement("SELECT * FROM " + tableName);
+               ResultSet sQLResultSet =sQLPreparedStatement.executeQuery();
+               ResultSetMetaData dataResultSetMetaData = sQLResultSet.getMetaData();
+               StringBuilder insertQuery = new StringBuilder("INSERT INTO " + tableName + " VALUES (");
+               for (int i = 1; i <= dataResultSetMetaData.getColumnCount(); i++) {
+                   if (i > 1) {
+                       insertQuery.append(", ");
+                   }
+                   insertQuery.append("?");
+                   
+               }
+               insertQuery.append(")");
+               
+              //creation d'une instance de connection mysql
+               
+               Connection mySQLConnection = DriverManager.getConnection(mySqlUrl, mySqlUser, mySqlPassword);
+              
+               //gestion de transaction des donées
+               mySQLConnection.setAutoCommit(false);
+               PreparedStatement mySQLPreparedStatement = mySQLConnection.prepareStatement(insertQuery.toString());
+               
+               try {
+            	   
+                   while (sQLResultSet.next()) {
+                       for (int i = 1; i <= dataResultSetMetaData.getColumnCount(); i++) {
+                    	   mySQLPreparedStatement.setObject(i, sQLResultSet.getObject(i));
+                       
+                       }
+                       
+                  //   insertStatement.executeUpdate();
+                       
+            	   
+               }
+                   mySQLConnection.commit();
+                   
+               }catch (Exception e) {
+            	   mySQLConnection.rollback();
+            	   	e.printStackTrace();
+			} finally {
+				mySQLPreparedStatement.close();
+				mySQLConnection.close();
+				sQLResultSet.close();
+				sQLPreparedStatement.close();
+				sQLTablesResultSet.close();
+				sQLServerConnection.close();
+			}
+           } 
+       }catch (Exception e) {
+    	   e.printStackTrace();
+	}
+        	
+        	
             
          
-        } catch (SQLException e) {
-        	
-            e.printStackTrace();
-        }
+       
     }
 }
