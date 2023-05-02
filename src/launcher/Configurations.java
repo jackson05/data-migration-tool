492229b996536841e3e18a519246b9b9cb840782
jackson05 <jackson5g.bi@gmail.com>
2023-05-02 13:24:25 +0200

    optimization of cod

--- src/launcher/Configurations.java
+++ src/launcher/Configurations.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import com.mysql.jdbc.Connection;
 
 public class Configurations {
 	
